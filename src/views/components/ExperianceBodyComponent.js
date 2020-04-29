ea880862af90f937f6bf3adc1a6ac5a05fcdad2e
Kielvien L <for2university@gmail.com>
2020-04-30 06:11:44 +0700

    initial experiance page feature

---
+++ src/views/components/ExperianceBodyComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ExperianceBody = (props) => {
+    return(
+
+    );
+}
+
+export default ExperianceBody;
---
+++ src/views/pages/ExperiancePage.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import ExperianceBodyComponenet from '../components/ExperianceBodyComponent';
+
+class ExperiancePage extends Component {
+    state = {
+        content: {
+            id: 2,
+            header: "Experiance",
+            description: "I have been around 2 year work as programmer since 2016 until now. " +
+                "My past archive project in that time variatif from small project to large project,"+
+                "as full stack developer or as spesific role. "+
+                "Right now I work as junior backend developer at one of consultant company in Indonesia. ",
+            resourcePath: require('../resource/Rem-example.jpg'),
+            resourceType: "jpg"
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <ExperianceBodyComponenet key={this.state.content.id} content={this.state.content} />
+            </div>
+        );
+    }
+}
+
+export default ExperiancePage;
